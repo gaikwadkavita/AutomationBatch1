2d9226e9b9306a696ae2951c5d0fd6531bd88300
Kavita Gaikwad <kavita.gaikwad@intellectdesign.com>
2020-09-05 14:03:58 +0530

    Fist Time commit on git

---
+++ BasicCoreJava/src/variablesAndDataTypes/DataTypes.java
@@ -0,0 +1,22 @@
+package variablesAndDataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age=35;
+		char gender='F';
+		String name="Kavita";
+		boolean maritalStatus=false;
+		double height=5.6;
+		
+		System.out.println(name);
+		
+		
+		
+		
+
+	}
+
+}
